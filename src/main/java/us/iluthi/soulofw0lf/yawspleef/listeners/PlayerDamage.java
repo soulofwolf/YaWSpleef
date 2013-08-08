e0b9c7774a36605583cf41a6074a0ecbcf6587de
soulofwolf <web_wizardz@mail.com>
2013-08-08 14:29:53 -0500

    - all "spell" events done

--- src/main/java/us/iluthi/soulofw0lf/yawspleef/listeners/PlayerDamage.java
+++ src/main/java/us/iluthi/soulofw0lf/yawspleef/listeners/PlayerDamage.java
@@ -1,8 +1,17 @@
 package us.iluthi.soulofw0lf.yawspleef.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import us.iluthi.soulofw0lf.yawspleef.Arena;
+import us.iluthi.soulofw0lf.yawspleef.Gun;
+import us.iluthi.soulofw0lf.yawspleef.YaWSpleef;
 
 /**
  * Created by: soulofw0lf
@@ -27,5 +36,36 @@ import org.bukkit.plugin.Plugin;
 public class PlayerDamage implements Listener {
     public PlayerDamage(Plugin plug){
         Bukkit.getPluginManager().registerEvents(this, plug);
+    }
+    @EventHandler
+    public void playerDamaged(EntityDamageByEntityEvent event){
+        if (event.getEntity() instanceof Player){
+            Player p = (Player)event.getEntity();
+            if (YaWSpleef.playerArenas.containsKey(p.getName())){
+                Arena a = YaWSpleef.spleefArenas.get(YaWSpleef.playerArenas.get(p.getName()));
+                if (event.getDamager() instanceof Arrow){
+                    event.setDamage(0);
+                    Arrow arrow = (Arrow)event.getDamager();
+                    if (arrow.getShooter() instanceof Player){
+                        Player shooter = (Player)arrow.getShooter();
+                        if (YaWSpleef.playerShots.containsKey(shooter.getName())){
+                            Gun g = YaWSpleef.guns.get(YaWSpleef.playerShots.get(shooter.getName()));
+                            if (YaWSpleef.blindS.contains(shooter.getName())){
+                                p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, g.getBlindingShotDuration()*20, 1, true));
+                                YaWSpleef.blindS.remove(shooter.getName());
+                            }
+                            if (YaWSpleef.slowS.contains(shooter.getName())){
+                                p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, g.getSlowDuration()*20, g.getSlowStr(), true));
+                                YaWSpleef.slowS.remove(shooter.getName());
+                            }
+                            if (YaWSpleef.summonS.contains(shooter.getName())){
+                                p.teleport(shooter.getLocation());
+                                YaWSpleef.summonS.remove(shooter.getName());
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
